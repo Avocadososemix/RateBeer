b1c950661d46b98c56a5c60a538968ba46fd2d20
Lauri Karanko <lauri.karanko@gmail.com>
2018-10-21 21:02:28 +0300

    added fragmentation caching

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -7,11 +7,12 @@ class BeersController < ApplicationController
   # GET /beers
   # GET /beers.json
   def index
+    @order = params[:order] || 'name'
+    # jos fragmentti olemassa, lopetetaan metodi tähän (eli renderöidään heti näkymä)
+    return if request.format.html? && fragment_exist?("beerlist-#{@order}")
+
     @beers = Beer.includes(:brewery, :style).all
-
-    order = params[:order] || 'name'
-
-    @beers = case order
+    @beers = case @order
       when 'name' then @beers.sort_by{ |b| b.name }
       when 'brewery' then @beers.sort_by{ |b| b.brewery.name }
       when 'style' then @beers.sort_by{ |b| b.style.name }
@@ -40,6 +41,7 @@ class BeersController < ApplicationController
   # POST /beers
   # POST /beers.json
   def create
+    ["beerlist-name", "beerlist-brewery", "beerlist-style"].each{ |f| expire_fragment(f) }
     @beer = Beer.new(beer_params)
 
     respond_to do |format|
@@ -56,6 +58,7 @@ class BeersController < ApplicationController
   # PATCH/PUT /beers/1
   # PATCH/PUT /beers/1.json
   def update
+    ["beerlist-name", "beerlist-brewery", "beerlist-style"].each{ |f| expire_fragment(f) }
     respond_to do |format|
       if @beer.update(beer_params)
         format.html { redirect_to @beer, notice: 'Beer was successfully updated.' }
@@ -70,6 +73,7 @@ class BeersController < ApplicationController
   # DELETE /beers/1
   # DELETE /beers/1.json
   def destroy
+    ["beerlist-name", "beerlist-brewery", "beerlist-style"].each{ |f| expire_fragment(f) }
     @beer.destroy
     respond_to do |format|
       format.html { redirect_to beers_url, notice: 'Beer was successfully destroyed.' }
